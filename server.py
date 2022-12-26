67db9b8a68c6231dc8a6188b60732608bf25c409
pkzrnvch <pkazarnovich@gmail.com>
2022-12-26 11:20:14 +0500

    Stop zip process at connection error

--- server.py
+++ server.py
@@ -1,11 +1,15 @@
 import asyncio
+import logging
+import os
 
 from aiohttp import web
 import aiofiles
-import os
 
 CHUNK_SIZE = 256 * 1024
 PHOTOS_GENERAL_DIRECTORY_PATH = 'test_photos'
+DOWNLOAD_DELAY = 0
+
+logger = logging.getLogger(__name__)
 
 
 async def archive(request):
@@ -18,7 +22,7 @@ async def archive(request):
     if not os.path.exists(requested_photos_directory_path):
         raise web.HTTPNotFound(reason='Архив не существует или был удален')
 
-    process = await asyncio.create_subprocess_exec(
+    zip_process = await asyncio.create_subprocess_exec(
         'zip',
         '-r',
         '-',
@@ -33,12 +37,21 @@ async def archive(request):
     await response.prepare(request)
 
     while True:
-        data_chunk = await process.stdout.read(CHUNK_SIZE)
-        await response.write(data_chunk)
-        if not data_chunk:
-            break
-
-    return response
+        try:
+            data_chunk = await zip_process.stdout.read(CHUNK_SIZE)
+            logger.debug('Sending archive chunk ...')
+            await response.write(data_chunk)
+            await asyncio.sleep(DOWNLOAD_DELAY)
+            if not data_chunk:
+                break
+        except asyncio.CancelledError:
+            logger.warning('Download was interrupted')
+            zip_process.kill()
+            logger.warning('Zip process was killed')
+            await zip_process.communicate()
+            raise
+        finally:
+            return response
 
 
 async def handle_index_page(request):
@@ -48,6 +61,11 @@ async def handle_index_page(request):
 
 
 if __name__ == '__main__':
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format='%(asctime)s - %(levelname)s - %(name)s - %(message)s',
+    )
+
     app = web.Application()
     app.add_routes([
         web.get('/', handle_index_page),
